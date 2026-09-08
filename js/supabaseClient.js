6d13f3a655643d9433dbbd33efc277b07038ff38
Gustaavo <gustavohenrique141312@gmail.com>
2026-09-08 18:22:52 -0300

    Beta

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,7 @@
 const SUPABASE_URL = "https://ahstdxgglfgjfeuurkqg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoc3RkeGdnbGZnamZldXVya3FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTY1ODIsImV4cCI6MjEwNDEzMjU4Mn0.4i7i3_w0n6VvrAXHduUMRKQPCQ6cR2SzAy7XCYtnUJY";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+if (!window.supabaseClientInstance) {
+    window.supabaseClientInstance = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+}
+const supabase = window.supabaseClientInstance;
