0d22b2a6be4d365c43b010dbe15d66fec3e5f721
Gustaavo <gustavohenrique141312@gmail.com>
2026-09-08 18:55:57 -0300

    Juliett

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,8 +1,9 @@
-const SUPABASE_URL = "https://ahstdxgglfgjfeuurkqg.supabase.co";
+(function () {
+    const url = "https://ahstdxgglfgjfeuurkqg.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoc3RkeGdnbGZnamZldXVya3FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTY1ODIsImV4cCI6MjEwNDEzMjU4Mn0.4i7i3_w0n6VvrAXHduUMRKQPCQ6cR2SzAy7XCYtnUJY";
+    const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoc3RkeGdnbGZnamZldXVya3FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTY1ODIsImV4cCI6MjEwNDEzMjU4Mn0.4i7i3_w0n6VvrAXHduUMRKQPCQ6cR2SzAy7XCYtnUJY";
 
-window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
+    if (!window.supabaseClient) {
+        window.supabaseClient = window.supabase.createClient(url, key);
+    }
+})();
