f077e72815dc12bcfe707de1093b2380e2fef3d9
Gustaavo <gustavohenrique141312@gmail.com>
2026-09-10 18:18:20 -0300

    Romeo

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
     "https://ahstdxgglfgjfeuurkqg.supabase.co";
 
 const SUPABASE_ANON_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhaHN0ZHhnbGZnamZldXVya3FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTY1ODIsImV4cCI6MjEwNDEzMjU4Mn0.4i7i3_w0n6VvrAXHduUMRKQPCQ6cR2SzAy7XCYtnUJY";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoc3RkeGdnbGZnamZldXVya3FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTY1ODIsImV4cCI6MjEwNDEzMjU4Mn0.4i7i3_w0n6VvrAXHduUMRKQPCQ6cR2SzAy7XCYtnUJY";
 
 window.meuClienteSupabase =
     window.supabase.createClient(
